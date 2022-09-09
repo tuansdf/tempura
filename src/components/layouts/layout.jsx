bdc7508e214cd50cdb22e489e690771c85793530
tuansdf <103062682+tuansdf@users.noreply.github.com>
2022-09-10 03:38:39 +0700

    feat: pwa

--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -4,11 +4,22 @@ import Header from "/src/components/layouts/header";
 
 export default function Layout() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <div className="container max-w-screen-md py-4">
+
+      <div className="container max-w-screen-md flex-1 py-4">
         <Outlet />
       </div>
-    </>
+
+      <div>
+        {/* website's icon */}
+        <a
+          href="https://www.flaticon.com/free-stickers/food"
+          title="food stickers"
+        >
+          Food stickers created by Adib Sulthon - Flaticon
+        </a>
+      </div>
+    </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "tempura.jpg"],
+      manifest: {
+        name: "Tempura",
+        short_name: "Tempura",
+        description: "A frontend for Reddit",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
