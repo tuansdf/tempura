18668a0fe64cc7769d0fb0c6a293e2f0e005b052
tuansdf <103062682+tuansdf@users.noreply.github.com>
2022-11-06 18:19:50 +0700

    feat: change to preact for build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react";
+import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    preact(),
     VitePWA({
       registerType: "autoUpdate",
       devOptions: {
